83c2ff9d90c1d62c756b7c4b7f0d7fee66054839
mau5atron <gabrielbetancourt96@outlook.com>
2018-02-18 03:45:48 -0800

    Updated sessions controller / made own version of clearance authenticate to create a form that allows sign in for both email and username inputs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+	private 
+
+	def authenticate(_)
+		super(session_params)
+		# inherits from session params
+	end
+
+	def session_params
+		{ session: session_params_with_email }
+	end
+
+	def session_params_with_email
+		params.
+		require(:session).
+		permit(:password).
+		merge(email: user.email)
+		# This will produce a hash that contains both password
+		# and email
+	end
+
+	def user
+		User.where(email: email_or_username)
+		.or(User.where(username: email_or_username))
+		.first || Guest.new
+	end
+
+	def email_or_username
+		params[:session][:email_or_username]
+	end
 end
