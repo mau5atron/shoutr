bd82969176f46b7d9e4d88bdc6010b1a045a759b
mau5atron <gabrielbetancourt96@outlook.com>
2018-02-18 05:57:37 -0800

    Made shouts display in descending order // moved shout post render from shout partial from shout controller view folder

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
 	def show
 		@shout = Shout.new
-		@shouts = current_user.shouts
+		@shouts = current_user.shouts.order("created_at DESC")
 	end
 end
