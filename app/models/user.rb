d631bae145894ecf7e948aeb9e75b4ed816b1782
mau5atron <gabrielbetancourt96@outlook.com>
2018-02-18 04:59:10 -0800

    Added has_many shouts association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   include Clearance::User
   validates :username, presence: true, uniqueness: true
+
+  has_many :shouts, dependent: :destroy
+  # Association for users
+  # each user has many shouts (like tweets)
+  # dependent destroy - added to delete all shouts when
+  # user is deleted
 end
