d19e1808d8ad3a58692feef418c77c2bad933b72
mau5atron <gabrielbetancourt96@outlook.com>
2018-02-18 04:03:49 -0800

    Updated users controller to acccept new users with action and render template users/new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,18 +11,13 @@ class SessionsController < Clearance::SessionsController
 	end
 
 	def session_params_with_email
-		params.
-		require(:session).
-		permit(:password).
-		merge(email: user.email)
+	params.require(:session).permit(:password).merge(email: user.email)
 		# This will produce a hash that contains both password
 		# and email
 	end
 
 	def user
-		User.where(email: email_or_username)
-		.or(User.where(username: email_or_username))
-		.first || Guest.new
+		User.where(email: email_or_username).or(User.where(username: email_or_username)).first || Guest.new
 	end
 
 	def email_or_username
