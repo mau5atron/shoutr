808fcfe0fa74168a1912ffd7ce76cfc4fc0346ce
mau5atron <gabrielbetancourt96@outlook.com>
2018-02-26 11:44:00 -0800

    Updated user model for liked shouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   # each user has many shouts (like tweets)
   # dependent destroy - added to delete all shouts when
   # user is deletedr
+
+  # user has many like and liked shouts
+  has_many :likes
+  has_many :liked_shouts, :through: :likes, source: :shout
+
+  def like(shout)
+  	liked_shouts << shout
+  end
 end
