b3a89da44d9b7649a8a4c6028c708acf9924c378
mau5atron <gabrielbetancourt96@outlook.com>
2018-02-27 00:09:07 -0800

    Added followed user shouts on current user dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
 	def show
 		@shout = Shout.new
-		@shouts = current_user.shouts.order("created_at DESC")
+		@shouts = current_user.timeline_shouts.order("created_at DESC")
 	end
 end
