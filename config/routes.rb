97f2127788581e8869b289c71b1342aa401c9fb1
mau5atron <gabrielbetancourt96@outlook.com>
2018-02-27 22:18:04 -0800

    Added ability to create hashtags

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,32 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
   end
 
-
+  # -----------------------------------
   root to: 'home#show'
   post "text_shouts" => 'shouts#create', defaults: { content_type: TextShout}
   # creates a text shout - cant be changed through form from default
   post "photo_shouts" => 'shouts#create', defaults: { content_type: PhotoShout}
-  
+
   resources :shouts, only: [:create, :show] do
     member do
       post "like" => 'likes#create'
       delete "unlike" => 'likes#destroy'
     end
   end
+  # -----------------------------------
 
 
+  # -----------------------------------
+  resources :hashtags, only: :show
+  # resources for hashtags
+  # -----------------------------------
+
+  # -----------------------------------
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
+  # -----------------------------------
 
+  # -----------------------------------
   resources :users, only: [:create, :show] do
     resources :followers, only: [:index]
     # new addition to user member - follow button group
@@ -31,8 +40,11 @@ Rails.application.routes.draw do
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
   end
+  # -----------------------------------
 
+  # -----------------------------------
   get "/sign_in" => "sessions#new", as: "sign_in"
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "users#new", as: "sign_up"
+  # -----------------------------------
 end
